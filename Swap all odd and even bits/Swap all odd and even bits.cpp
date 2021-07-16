0fbe4e3067d26753f44701d7288914bfebe12295
dheeru-karanam <dheerajkaranam1@gmail.com>
2021-07-16 21:28:46 +0530

    Swap all odd and even bits
    Program to Swap all odd and even bits
    in O(1) time constraint

---
+++ Swap all odd and even bits/Swap all odd and even bits.cpp
@@ -0,0 +1,42 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to swap odd and even bits.
+    unsigned int swapBits(unsigned int n)
+    {
+    	// Your code here
+    	// Your code here
+    	unsigned int mask1 = 0xAAAAAAAA;
+    	unsigned int mask2 = 0x55555555;
+    	return ((n&mask1)>>1) | ((n&mask2)<<1);
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;//testcases
+	while(t--)
+	{
+		unsigned int n;
+		cin>>n;//input n
+		
+		Solution ob;
+		//calling swapBits() method
+		cout << ob.swapBits(n) << endl;
+	}
+	return 0;
+}  // } Driver Code Ends
