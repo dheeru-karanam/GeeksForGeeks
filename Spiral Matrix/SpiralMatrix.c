55a7f49dfde639632ae19da87ebb3821f218776e
dheerajkaranam1@gmail.com <dheerajkaranam1@gmail.com>
2021-07-25 23:22:01 +0530

    Reading array
    Added to read array and perform the operation

--- Spiral Matrix/SpiralMatrix.c
+++ Spiral Matrix/SpiralMatrix.c
@@ -1,11 +1,24 @@
 #include "stdio.h"
 int main(){
 	int top,bottom,left,right;
-	int arr[4][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+	int row, column;
+	scanf("%d%d", &row, &column);
+	int arr[row][column];
+	for(int i=0;i<row;i++){
+		for(int j=0;j<column;j++){
+			scanf("%d", &arr[i][j]);
+		}
+	}
+	for(int i=0;i<row;i++){
+		for(int j=0;j<column;j++){
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
 	top = 0;
-	bottom = 3;
+	bottom = row-1;
 	left = 0;
-	right = 3;
+	right = column-1;
 	int dir = 1;
 	while(top<=bottom && left<=right){
 		if(dir%4==1){
