596da332ede57e41b9a66d9b8b36a020777ba457
dheerajkaranam1@gmail.com <dheerajkaranam1@gmail.com>
2021-07-27 21:27:39 +0530

    Reading Rows and Columns
    Update to the existing code

--- ZigZagArray/ZigZagMatrix.c
+++ ZigZagArray/ZigZagMatrix.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 int main(){
 	int i,j,k;
-	int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+	int row,column;
+	scanf("%d%d",&row, &column);
+	int arr[row][column];
+	for(int i=0;i<row;i++){
+		for(int j=0;j<column;j++){
+			scanf("%d", &arr[i][j]);
+		}
+	}
 	int pointer=0;
-	while(pointer<3){
+	while(pointer<row){
 		if(pointer%2==0){
-			for(int i=2;i>=0;i--){
+			for(int i=column-1;i>=0;i--){
 				printf("%d ",arr[pointer][i]);
 			}
 			pointer++;
 			printf("\n");
 		}
 		else if(pointer%2==1){
-			for(int i=0;i<3;i++){
+			for(int i=0;i<column;i++){
 				printf("%d ",arr[pointer][i]);
 			}
 			pointer++;
